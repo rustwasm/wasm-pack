83e44c19b8cd6474cf569ae6a0e724065792d2ca
Ashley Williams <ashley666ashley@gmail.com>
2018-03-02 17:37:41 -0500

    feat(args): take a path to a crate as an arg to init

--- tests/wasm-pack.rs
+++ tests/wasm-pack.rs
@@ -3,7 +3,13 @@ extern crate wasm_pack;
 use std::fs;
 
 #[test]
-fn it_creates_a_package_json() {
-    assert!(wasm_pack::write_package_json().is_ok());
+fn it_creates_a_package_json_default_path() {
+    assert!(wasm_pack::write_package_json(".").is_ok());
+    assert!(fs::metadata("./pkg/package.json").is_ok());
+}
+
+#[test]
+fn it_creates_a_package_json_provided_path() {
+    assert!(wasm_pack::write_package_json("./examples/js-hello-world").is_ok());
     assert!(fs::metadata("./pkg/package.json").is_ok());
 }
