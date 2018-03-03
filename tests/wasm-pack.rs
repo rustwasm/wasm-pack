a305a4fcceec8ef9ddd7bbf63a9d6074db9d3307
Ashley Williams <ashley666ashley@gmail.com>
2018-03-02 19:33:44 -0500

    feat(test): does it get the right crate name?

--- tests/wasm-pack.rs
+++ tests/wasm-pack.rs
@@ -3,6 +3,18 @@ extern crate wasm_pack;
 use std::fs;
 
 #[test]
+fn it_gets_the_crate_name_default_path() {
+  assert!(wasm_pack::get_crate_name(".").is_ok());
+  assert_eq!(wasm_pack::get_crate_name(".").unwrap(), "wasm-pack");
+}
+
+#[test]
+fn it_gets_the_crate_name_provided_path() {
+  assert!(wasm_pack::get_crate_name("./examples/js-hello-world").is_ok());
+  assert_eq!(wasm_pack::get_crate_name(".").unwrap(), "js-hello-world");
+}
+
+#[test]
 fn it_creates_a_package_json_default_path() {
     assert!(wasm_pack::write_package_json(".").is_ok());
     assert!(fs::metadata("./pkg/package.json").is_ok());
