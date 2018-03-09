0518f7ba5a1b9fc79c6982cb044c045d7a78e5cc
Sendil Kumar <sendilkumarn@live.com>
2018-03-09 07:40:03 +0100

    rename variables

--- src/bindgen.rs
+++ src/bindgen.rs
@@ -27,10 +27,10 @@ pub fn cargo_install_wasm_bindgen() {
 
 pub fn wasm_bindgen_build(path: &str, name: &str) {
     let binary_name = name.replace("-", "_");
-    let path_to_wasm = format!("target/wasm32-unknown-unknown/release/{}.wasm", binary_name);
+    let wasm_path = format!("target/wasm32-unknown-unknown/release/{}.wasm", binary_name);
     let output = Command::new("wasm-bindgen")
         .current_dir(path)
-        .arg(&path_to_wasm)
+        .arg(&wasm_path)
         .arg("--out-dir")
         .arg("./pkg")
         .output()
