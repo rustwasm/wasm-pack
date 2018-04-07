2a996cb5545675da507546d97c8ab38cb0c0dd12
andy-bell <andy.bell.github@gmail.com>
2018-04-07 13:22:40 +0100

    feat(refactor): missed one warning

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use quicli::prelude::*;
 use wasm_pack::Cli;
 use wasm_pack::command::{init_command, pack_command, publish_command, Command};
 
-
 main!(|args: Cli, log_level: verbosity| match args.cmd {
     Command::Init { path, scope } => {
         init_command(path, scope)?;
