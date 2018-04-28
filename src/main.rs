3f28bd4142e8b46b6fe1d2ff7ea6c5a4149bbd8f
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-29 01:54:39 +0200

    pretty-print panics

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ extern crate wasm_pack;
 extern crate indicatif;
 #[macro_use]
 extern crate quicli;
+#[macro_use]
+extern crate human_panic;
 
 use quicli::prelude::*;
 use wasm_pack::command::run_wasm_pack;
 use wasm_pack::Cli;
 
 main!(|args: Cli, log_level: verbosity| {
+    setup_panic!();
     run_wasm_pack(args.cmd)?;
 });
