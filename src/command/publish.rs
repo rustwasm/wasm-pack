ec17b8aa7edcb9dd38c3142e82da360fcdcfdbb3
Mackiovello <elandroe@icloud.com>
2018-07-05 19:02:50 +0200

    Use custom error message for publish

--- src/command/publish.rs
+++ src/command/publish.rs
@@ -10,7 +10,13 @@ pub fn publish(path: Option<String>, log: &Logger) -> result::Result<(), Error> 
 
     info!(&log, "Publishing the npm package...");
     info!(&log, "npm info located in the npm debug log");
-    npm::npm_publish(&crate_path)?;
+    match npm::npm_publish(&crate_path) {
+        Ok(r) => Ok(r),
+        Err(Error::Io { .. }) => Err(Error::DirNotFound {
+            message: "Unable to find the pkg directory".to_owned(),
+        }),
+        Err(e) => Err(e),
+    }?;
     info!(&log, "Published your package!");
 
     PBAR.message("💥  published your package!");
