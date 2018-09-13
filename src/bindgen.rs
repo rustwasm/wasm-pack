c8aac6585d6c43c0897a5c4ce91179df862668c4
Nick Fitzgerald <fitzgen@gmail.com>
2018-09-13 16:16:31 -0700

    feature(test): Add a `wasm-pack test` subcommand
    This uses `wasm-bindgen-test` under the hood.
    
    Fixes #248

--- src/bindgen.rs
+++ src/bindgen.rs
@@ -1,18 +1,13 @@
 //! Functionality related to installing and running `wasm-bindgen`.
 
-use curl;
+use binaries::{self, bin_path, install_binaries_from_targz_at_url};
 use emoji;
 use error::Error;
-use failure;
-use flate2;
 use progressbar::Step;
 use slog::Logger;
-use std::ffi;
-use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use tar;
-use which::which;
+use target;
 use PBAR;
 
 /// Install the `wasm-bindgen` CLI.
@@ -29,8 +24,8 @@ pub fn install_wasm_bindgen(
     log: &Logger,
 ) -> Result<(), Error> {
     // If the `wasm-bindgen` dependency is already met, print a message and return.
-    if wasm_bindgen_path(root_path)
-        .map(|bindgen_path| wasm_bindgen_version_check(&bindgen_path, version))
+    if wasm_bindgen_path(log, root_path)
+        .map(|bindgen_path| wasm_bindgen_version_check(&bindgen_path, version, log))
         .unwrap_or(false)
     {
         let msg = format!("{}wasm-bindgen already installed...", emoji::DOWN_ARROW);
@@ -57,56 +52,13 @@ pub fn install_wasm_bindgen(
     })
 }
 
-fn curl(url: &str) -> Result<Vec<u8>, failure::Error> {
-    let mut data = Vec::new();
-
-    fn with_url_context<T, E>(url: &str, r: Result<T, E>) -> Result<T, impl failure::Fail>
-    where
-        Result<T, E>: failure::ResultExt<T, E>,
-    {
-        use failure::ResultExt;
-        r.with_context(|_| format!("when requesting {}", url))
-    }
-
-    let mut easy = curl::easy::Easy::new();
-    with_url_context(url, easy.follow_location(true))?;
-    with_url_context(url, easy.url(url))?;
-
-    {
-        let mut transfer = easy.transfer();
-        with_url_context(
-            url,
-            transfer.write_function(|part| {
-                data.extend_from_slice(part);
-                Ok(part.len())
-            }),
-        )?;
-        with_url_context(url, transfer.perform())?;
-    }
-
-    let code = with_url_context(url, easy.response_code())?;
-    if 200 <= code && code < 300 {
-        Ok(data)
-    } else {
-        Err(Error::http(&format!(
-            "received a bad HTTP status code ({}) when requesting {}",
-            code, url
-        )).into())
-    }
-}
-
 /// Download a tarball containing a pre-built `wasm-bindgen` binary.
 pub fn download_prebuilt_wasm_bindgen(root_path: &Path, version: &str) -> Result<(), Error> {
-    let linux = cfg!(target_os = "linux");
-    let macos = cfg!(target_os = "macos");
-    let windows = cfg!(windows);
-    let x86_64 = cfg!(target_arch = "x86_64");
-
-    let target = if linux && x86_64 {
+    let target = if target::LINUX && target::x86_64 {
         "x86_64-unknown-linux-musl"
-    } else if macos && x86_64 {
+    } else if target::MACOS && target::x86_64 {
         "x86_64-apple-darwin"
-    } else if windows && x86_64 {
+    } else if target::WINDOWS && target::x86_64 {
         "x86_64-pc-windows-msvc"
     } else {
         return Err(Error::unsupported(
@@ -120,45 +72,16 @@ pub fn download_prebuilt_wasm_bindgen(root_path: &Path, version: &str) -> Result
         target
     );
 
-    let tarball = curl(&url).map_err(|e| Error::http(&e.to_string()))?;
-    let mut archive = tar::Archive::new(flate2::read::GzDecoder::new(&tarball[..]));
-
-    let bin = root_path.join("bin");
-    fs::create_dir_all(&bin)?;
-
-    let mut found_wasm_bindgen = false;
-    let mut found_test_runner = false;
-
-    for entry in archive.entries()? {
-        let mut entry = entry?;
-
-        let dest = match entry.path()?.file_stem() {
-            Some(f) if f == ffi::OsStr::new("wasm-bindgen") => {
-                found_wasm_bindgen = true;
-                bin.join(entry.path()?.file_name().unwrap())
-            }
-            Some(f) if f == ffi::OsStr::new("wasm-bindgen-test-runner") => {
-                found_test_runner = true;
-                bin.join(entry.path()?.file_name().unwrap())
-            }
-            _ => continue,
-        };
-
-        entry.unpack(dest)?;
-    }
-
-    if found_wasm_bindgen && found_test_runner {
-        Ok(())
-    } else {
-        Err(Error::archive(
-            "the wasm-bindgen tarball was missing expected executables",
-        ))
-    }
+    install_binaries_from_targz_at_url(
+        root_path,
+        &url,
+        vec!["wasm-bindgen", "wasm-bindgen-test-runner"],
+    )
 }
 
-/// Use `cargo install` to install the `wasm-bindgen` CLI to the given root
-/// path.
-pub fn cargo_install_wasm_bindgen(root_path: &Path, version: &str) -> Result<(), Error> {
+/// Use `cargo install` to install the `wasm-bindgen` CLI locally into the given
+/// crate.
+pub fn cargo_install_wasm_bindgen(crate_path: &Path, version: &str) -> Result<(), Error> {
     let output = Command::new("cargo")
         .arg("install")
         .arg("--force")
@@ -166,7 +89,7 @@ pub fn cargo_install_wasm_bindgen(root_path: &Path, version: &str) -> Result<(),
         .arg("--version")
         .arg(version)
         .arg("--root")
-        .arg(root_path)
+        .arg(crate_path)
         .output()?;
     if !output.status.success() {
         let message = "Installing wasm-bindgen failed".to_string();
@@ -176,11 +99,7 @@ pub fn cargo_install_wasm_bindgen(root_path: &Path, version: &str) -> Result<(),
             stderr: s.to_string(),
         })
     } else {
-        if cfg!(target_os = "windows") {
-            assert!(root_path.join("bin").join("wasm-bindgen.exe").is_file());
-        } else {
-            assert!(root_path.join("bin").join("wasm-bindgen").is_file());
-        }
+        assert!(binaries::local_bin_path(crate_path, "wasm-bindgen").is_file());
         Ok(())
     }
 }
@@ -195,6 +114,7 @@ pub fn wasm_bindgen_build(
     target: &str,
     debug: bool,
     step: &Step,
+    log: &Logger,
 ) -> Result<(), Error> {
     let msg = format!("{}Running WASM-bindgen...", emoji::RUNNER);
     PBAR.step(step, &msg);
@@ -204,7 +124,7 @@ pub fn wasm_bindgen_build(
 
     let out_dir = out_dir.to_str().unwrap();
 
-    if let Some(wasm_bindgen_path) = wasm_bindgen_path(path) {
+    if let Some(wasm_bindgen_path) = wasm_bindgen_path(log, path) {
         let wasm_path = format!(
             "target/wasm32-unknown-unknown/{}/{}.wasm",
             release_or_debug, binary_name
@@ -239,7 +159,7 @@ pub fn wasm_bindgen_build(
 }
 
 /// Check if the `wasm-bindgen` dependency is locally satisfied.
-fn wasm_bindgen_version_check(bindgen_path: &PathBuf, dep_version: &str) -> bool {
+fn wasm_bindgen_version_check(bindgen_path: &PathBuf, dep_version: &str, log: &Logger) -> bool {
     Command::new(bindgen_path)
         .arg("--version")
         .output()
@@ -250,38 +170,27 @@ fn wasm_bindgen_version_check(bindgen_path: &PathBuf, dep_version: &str) -> bool
                 .trim()
                 .split_whitespace()
                 .nth(1)
-                .map(|v| v == dep_version)
-                .unwrap_or(false)
+                .map(|v| {
+                    info!(
+                        log,
+                        "Checking installed `wasm-bindgen` version == expected version: {} == {}",
+                        v,
+                        dep_version
+                    );
+                    v == dep_version
+                }).unwrap_or(false)
         }).unwrap_or(false)
 }
 
 /// Return a `PathBuf` containing the path to either the local wasm-bindgen
 /// version, or the globally installed version if there is no local version.
-fn wasm_bindgen_path(crate_path: &Path) -> Option<PathBuf> {
-    // Return the path to the local `wasm-bindgen`, if it exists.
-    let local_bindgen_path = |crate_path: &Path| -> Option<PathBuf> {
-        let mut p = crate_path.to_path_buf();
-        p.push("bin");
-        if cfg!(target_os = "windows") {
-            p.push("wasm-bindgen.exe");
-        } else {
-            p.push("wasm-bindgen");
-        }
-        if p.is_file() {
-            Some(p)
-        } else {
-            None
-        }
-    };
+fn wasm_bindgen_path(log: &Logger, crate_path: &Path) -> Option<PathBuf> {
+    bin_path(log, crate_path, "wasm-bindgen")
+}
 
-    // Return the path to the global `wasm-bindgen`, if it exists.
-    let global_bindgen_path = || -> Option<PathBuf> {
-        if let Ok(p) = which("wasm-bindgen") {
-            Some(p)
-        } else {
-            None
-        }
-    };
-
-    local_bindgen_path(crate_path).or_else(global_bindgen_path)
+/// Return a `PathBuf` containing the path to either the local
+/// wasm-bindgen-test-runner version, or the globally installed version if there
+/// is no local version.
+pub fn wasm_bindgen_test_runner_path(log: &Logger, crate_path: &Path) -> Option<PathBuf> {
+    bin_path(log, crate_path, "wasm-bindgen-test-runner")
 }
