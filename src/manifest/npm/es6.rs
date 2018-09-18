f06f3cca0d3238e3da2b5ca333aee0657b7dbdd9
Ashley Williams <ashley666ashley@gmail.com>
2018-09-18 17:32:49 -0400

    refactor(manifest/npm): break into mods

---
+++ src/manifest/npm/es6.rs
@@ -0,0 +1,20 @@
+use manifest::npm::repository::Repository;
+
+#[derive(Serialize)]
+pub struct ES6Package {
+    pub name: String,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub collaborators: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    pub version: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub license: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub repository: Option<Repository>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub files: Vec<String>,
+    pub module: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub types: Option<String>,
+}
