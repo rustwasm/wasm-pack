49f12061843d8eae8e08872f2a6b1639c2e2c562
Ashley Williams <ashley666ashley@gmail.com>
2018-09-21 11:22:46 -0400

    feat(manifest:npm): add nomodules manifest struct

---
+++ src/manifest/npm/nomodules.rs
@@ -0,0 +1,20 @@
+use manifest::npm::repository::Repository;
+
+#[derive(Serialize)]
+pub struct NoModulesPackage {
+    pub name: String,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub collaborators: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    pub version: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub license: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub repository: Option<Repository>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub files: Vec<String>,
+    pub browser: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub types: Option<String>,
+}
