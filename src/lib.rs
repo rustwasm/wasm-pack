a797aa0583c540b6e629aff53e1dea740052f599
Spencer Wilson <5624115+spencerwilson@users.noreply.github.com>
2018-09-23 10:36:34 -0700

    fix(bindgen): Test installed version with semver crate

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ extern crate indicatif;
 #[macro_use]
 extern crate lazy_static;
 extern crate parking_lot;
+extern crate semver;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
