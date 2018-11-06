714a8a1581bf31fb89c5c849b8b2bc4735d84063
Jesper Håkansson <jesper@jesperh.se>
2018-11-06 20:43:37 +0100

    refactor: Remove unneeded code, since master has changed a lot
    The modules and tests are no longer relevant since master has changed
    a lot since the code was split into smalled modules.

--- binary-install/src/target.rs
+++
@@ -1,8 +0,0 @@
-//! Information about the target wasm-pack is currently being compiled for.
-//!
-//! That is, whether we are building wasm-pack for windows vs linux, and x86 vs
-//! x86-64, etc.
-
-#![allow(missing_docs)]
-
-pub const WINDOWS: bool = cfg!(target_os = "windows");
