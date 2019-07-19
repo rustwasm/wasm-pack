5218be592cd4c90f8c9d13d9789e5aaf9b217efb
Ashley Williams <ashley666ashley@gmail.com>
2019-07-19 10:21:42 -0500

    refactor(tool): wasm-opt is a tool variant

--- src/wasm_opt.rs
+++ src/wasm_opt.rs
@@ -1,12 +1,10 @@
 //! Support for downloading and executing `wasm-opt`
 
 use crate::child;
-use crate::emoji;
-use crate::target;
+use crate::install::{self, Tool};
 use crate::PBAR;
 use binary_install::Cache;
-use log::debug;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::Command;
 
 /// Execute `wasm-opt` over wasm binaries found in `out_dir`, downloading if
@@ -18,17 +16,18 @@ pub fn run(
     install_permitted: bool,
 ) -> Result<(), failure::Error> {
     let wasm_opt = match find_wasm_opt(cache, install_permitted)? {
-        WasmOpt::Found(path) => path,
-        WasmOpt::CannotInstall => {
+        install::Status::Found(path) => path,
+        install::Status::CannotInstall => {
             PBAR.info("Skipping wasm-opt as no downloading was requested");
             return Ok(());
         }
-        WasmOpt::PlatformNotSupported => {
+        install::Status::PlatformNotSupported => {
             PBAR.info("Skipping wasm-opt because it is not supported on this platform");
             return Ok(());
         }
     };
 
+    let wasm_opt_path = wasm_opt.binary(&Tool::WasmOpt.to_string())?;
     PBAR.info("Optimizing wasm binaries with `wasm-opt`...");
 
     for file in out_dir.read_dir()? {
@@ -39,23 +38,13 @@ pub fn run(
         }
 
         let tmp = path.with_extension("wasm-opt.wasm");
-        let mut cmd = Command::new(&wasm_opt);
+        let mut cmd = Command::new(&wasm_opt_path);
         cmd.arg(&path).arg("-o").arg(&tmp).args(args);
         child::run(cmd, "wasm-opt")?;
         std::fs::rename(&tmp, &path)?;
     }
 
     Ok(())
-}
-
-/// Possible results of `find_wasm_opt`
-pub enum WasmOpt {
-    /// Couldn't install wasm-opt because downloads are forbidden
-    CannotInstall,
-    /// The current platform doesn't support precompiled binaries
-    PlatformNotSupported,
-    /// We found `wasm-opt` at the specified path
-    Found(PathBuf),
 }
 
 /// Attempts to find `wasm-opt` in `PATH` locally, or failing that downloads a
@@ -65,44 +54,15 @@ pub enum WasmOpt {
 /// Returns `None` if a binary wasn't found in `PATH` and this platform doesn't
 /// have precompiled binaries. Returns an error if we failed to download the
 /// binary.
-pub fn find_wasm_opt(cache: &Cache, install_permitted: bool) -> Result<WasmOpt, failure::Error> {
-    // First attempt to look up in PATH. If found assume it works.
-    if let Ok(path) = which::which("wasm-opt") {
-        debug!("found wasm-opt at {:?}", path);
-        return Ok(WasmOpt::Found(path));
-    }
-
-    // ... and if that fails download a precompiled version.
-    let target = if target::LINUX && target::x86_64 {
-        "x86_64-linux"
-    } else if target::MACOS && target::x86_64 {
-        "x86_64-apple-darwin"
-    } else if target::WINDOWS && target::x86_64 {
-        "x86_64-windows"
-    } else {
-        return Ok(WasmOpt::PlatformNotSupported);
-    };
-    let url = format!(
-        "https://github.com/WebAssembly/binaryen/releases/download/{vers}/binaryen-{vers}-{target}.tar.gz",
-        vers = "version_78",
-        target = target,
-    );
-
-    let download = |permit_install| cache.download(permit_install, "wasm-opt", &["wasm-opt"], &url);
-
-    let dl = match download(false)? {
-        Some(dl) => dl,
-        None if !install_permitted => return Ok(WasmOpt::CannotInstall),
-        None => {
-            let msg = format!("{}Installing wasm-opt...", emoji::DOWN_ARROW);
-            PBAR.info(&msg);
-
-            match download(install_permitted)? {
-                Some(dl) => dl,
-                None => return Ok(WasmOpt::CannotInstall),
-            }
-        }
-    };
-
-    Ok(WasmOpt::Found(dl.binary("wasm-opt")?))
+pub fn find_wasm_opt(
+    cache: &Cache,
+    install_permitted: bool,
+) -> Result<install::Status, failure::Error> {
+    let version = "version_78";
+    Ok(install::download_prebuilt(
+        &install::Tool::WasmOpt,
+        cache,
+        version,
+        install_permitted,
+    )?)
 }
