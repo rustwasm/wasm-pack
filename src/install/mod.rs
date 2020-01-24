f7c5f3d4191c1765a53775504ff80be54101c0a8
Ashley Williams <ashley666ashley@gmail.com>
2020-01-24 10:23:22 -0600

    fix(tool): wasm-opt windows using diff target string

--- src/install/mod.rs
+++ src/install/mod.rs
@@ -164,13 +164,16 @@ pub fn download_prebuilt(
 fn prebuilt_url(tool: &Tool, version: &str) -> Result<String, failure::Error> {
     let target = if target::LINUX && target::x86_64 {
         match tool {
-            Tool::WasmOpt => "x86-linux",
+            Tool::WasmOpt => "x86_64-linux",
             _ => "x86_64-unknown-linux-musl",
         }
     } else if target::MACOS && target::x86_64 {
         "x86_64-apple-darwin"
     } else if target::WINDOWS && target::x86_64 {
-        "x86_64-pc-windows-msvc"
+        match tool {
+        Tool::WasmOpt => "x86-windows",
+        _ => "x86_64-pc-windows-msvc",
+        }
     } else {
         bail!("Unrecognized target!")
     };
