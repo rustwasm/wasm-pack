77099ca6b2f51754ae38e0c1616700059880c6e1
Frederic Kettelhoit <kettelhoit@gmail.com>
2021-08-07 14:35:56 +0200

    Update cargo_metadata dependency to 0.14.0

--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -426,7 +426,7 @@ impl CrateData {
             .iter()
             .position(|pkg| {
                 pkg.name == manifest.package.name
-                    && CrateData::is_same_path(&pkg.manifest_path, &manifest_path)
+                    && CrateData::is_same_path(&pkg.manifest_path.as_std_path(), &manifest_path)
             })
             .ok_or_else(|| format_err!("failed to find package in metadata"))?;
 
