113f47e4da0ffe317eb2c460322e802782dd26d0
Samuel Cristobal <el.tobal@gmail.com>
2023-02-13 20:31:47 -0700

    more concise

--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -628,14 +628,10 @@ impl CrateData {
         let dts_file = if !disable_dts {
             let file = format!("{}.d.ts", name_prefix);
             files.push(file.to_string());
-            Some(file)
-        } else {
-            None
-        };
 
-        if !disable_dts {
-            let file = format!("{}_bg.wasm.d.ts", name_prefix);
-            files.push(file.to_string());
+            let wasm_def_file = format!("{}_bg.wasm.d.ts", name_prefix);
+            files.push(wasm_def_file);
+
             Some(file)
         } else {
             None
