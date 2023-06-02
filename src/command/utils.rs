d61822b4af738a95d307d481d2f4b1dc8347e2e9
dullbananas <dull.bananas0@gmail.com>
2023-06-02 13:53:40 +0000

    Revert "Refactor run_step"
    This reverts commit 130e237833e09cae1aef6e8bcebe838b81dc810b.

--- src/command/utils.rs
+++ src/command/utils.rs
@@ -73,16 +73,11 @@ pub fn elapsed(duration: Duration) -> String {
 
 /// Runs a funcion and reports duration if `perf` feature is enabled
 pub fn run_step<T, U>((name, mut f): (&'static str, impl FnMut(T) -> U), arg: T) -> U {
+    let start = std::time::Instant::now();
+    let result = f(arg);
     #[cfg(feature = "perf")]
-    {
-        let start = std::time::Instant::now();
-        let result = f(arg);
-        println!("{}: {} s", name, start.elapsed().as_secs_f64());
-        result
-    }
+    println!("{}: {} s", name, start.elapsed().as_secs_f64());
     #[cfg(not(feature = "perf"))]
-    {
-        let _ = name;
-        f(arg)
-    }
+    let _ = name;
+    result
 }
