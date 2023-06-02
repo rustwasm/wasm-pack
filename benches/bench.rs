1dd7e7836f1919595384cc52982d59e4ce2b5de9
dullbananas <dull.bananas0@gmail.com>
2023-06-02 13:53:58 +0000

    Revert "increase warm up time"
    This reverts commit dd71328c0f20da00e7f54460e95efdd3957a86ef.

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,11 +5,9 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::process::Stdio;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut g = c.benchmark_group("wasm-pack");
-    g.warm_up_time(std::time::Duration::from_secs(10));
     let fixture = fixture::dual_license();
     run(&fixture);
-    g.bench_function("re-run build without code changes", |b| {
+    c.bench_function("re-run build without code changes", |b| {
         b.iter(|| run(&fixture))
     });
 }
