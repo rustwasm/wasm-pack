b6d095c73f9edad53a9047031481bc1cf7bc34e6
dullbananas <dull.bananas0@gmail.com>
2023-06-03 22:30:46 +0000

    Add dhat heap profiler

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,14 @@ fn background_check_for_updates() -> mpsc::Receiver<Result<WasmPackVersion>> {
     receiver
 }
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     env_logger::init();
 
     setup_panic_hooks();
