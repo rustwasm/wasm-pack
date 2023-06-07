7f395068e10dfd4f33e306c0e8bf951354955b50
dullbananas <dull.bananas0@gmail.com>
2023-06-07 04:15:58 +0000

    Add args directly in npm_login

--- src/npm.rs
+++ src/npm.rs
@@ -33,20 +33,18 @@ pub fn npm_publish(path: &str, access: Option<Access>, tag: Option<String>) -> R
 
 /// Run the `npm login` command.
 pub fn npm_login(registry: &str, scope: &Option<String>, auth_type: &Option<String>) -> Result<()> {
-    let mut args = vec!["login".to_string(), format!("--registry={}", registry)];
-
-    if let Some(scope) = scope {
-        args.push(format!("--scope={}", scope));
-    }
-
-    if let Some(auth_type) = auth_type {
-        args.push(format!("--auth_type={}", auth_type));
-    }
-
     // Interactively ask user for npm login info.
     //  (child::run does not support interactive input)
     let mut cmd = child::new_command("npm");
-    cmd.args(args);
+    cmd.args(["login".to_string(), format!("--registry={}", registry)]);
+
+    if let Some(scope) = scope {
+        cmd.arg(format!("--scope={}", scope));
+    }
+
+    if let Some(auth_type) = auth_type {
+        cmd.arg(format!("--auth_type={}", auth_type));
+    }
 
     info!("Running {:?}", cmd);
     if cmd.status()?.success() {
