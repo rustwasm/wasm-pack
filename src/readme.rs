927e716b87c69323375edb76438e1dd1e64fbda4
Jesper Håkansson <jesper@jesperh.se>
2023-06-11 18:02:14 +0200

    Revert "Respect package.readme in Cargo.toml"
    This reverts commit 22d6d728d021691729ef8d9acf839ec315da79e7.

--- src/readme.rs
+++ src/readme.rs
@@ -4,11 +4,10 @@ use anyhow::{Context, Result};
 use std::fs;
 use std::path::Path;
 
-use crate::manifest::CrateData;
 use crate::PBAR;
 
 /// Copy the crate's README into the `pkg` directory.
-pub fn copy_from_crate(crate_data: &CrateData, path: &Path, out_dir: &Path) -> Result<()> {
+pub fn copy_from_crate(path: &Path, out_dir: &Path) -> Result<()> {
     assert!(
         fs::metadata(path).ok().map_or(false, |m| m.is_dir()),
         "crate directory should exist"
@@ -18,11 +17,7 @@ pub fn copy_from_crate(crate_data: &CrateData, path: &Path, out_dir: &Path) -> R
         "crate's pkg directory should exist"
     );
 
-    let crate_readme_path = match crate_data.crate_readme() {
-        None => return Ok(()),
-        Some(readme_path) => path.join(readme_path),
-    };
-
+    let crate_readme_path = path.join("README.md");
     let new_readme_path = out_dir.join("README.md");
     if crate_readme_path.exists() {
         fs::copy(&crate_readme_path, &new_readme_path).context("failed to copy README")?;
