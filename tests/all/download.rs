f2a04cc93dc7b54fcc53049e2f1b53cbeb8a5a09
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-09-16 19:21:20 +0800

    Add negative test for version extraction

--- tests/all/download.rs
+++ tests/all/download.rs
@@ -3,17 +3,18 @@ use wasm_pack::install::{self, extract_version, Arch, Os, Tool};
 #[test]
 fn can_extract_cli_version() {
     let tests = [
-        ("cargo-generate 0.18.4", "0.18.4"),
-        ("wasm-bindgen 0.2.87", "0.2.87"),
-        ("wasm-opt version 116", "116"),
-        ("cargo-generate 1", "1"),       // missing minor & patch version
-        ("cargo-generate 0.18", "0.18"), // missing patch version
-        ("cargo generate 0.18.4", "0.18.4"), // space-separated subcommand
-        ("wasm-bindgen 0.2.87 (deadbeef)", "0.2.87"), // with commit hash
+        ("cargo-generate 0.18.4", Some("0.18.4")),
+        ("wasm-bindgen 0.2.87", Some("0.2.87")),
+        ("wasm-opt version 116", Some("116")),
+        ("cargo-generate 1", Some("1")), // missing minor & patch version
+        ("cargo-generate 0.18", Some("0.18")), // missing patch version
+        ("cargo generate 0.18.4", Some("0.18.4")), // space-separated subcommand
+        ("wasm-bindgen 0.2.87 (deadbeef)", Some("0.2.87")), // with commit hash
+        ("wasm-opt version", None),      // version missing
     ];
 
     for (i, o) in tests {
-        assert_eq!(extract_version(i), Some(o));
+        assert_eq!(extract_version(i), o);
     }
 }
 
